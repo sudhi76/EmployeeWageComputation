5072e284d07d07307c0c91b4b8064329ab731c35
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-05 22:27:30 +0530

    Added: empDailyWageLogic

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -7,15 +7,27 @@ public class EmployeeWageComputation {
 				+ "Welcome to Employee\r\n"
 				+ "Wage Computation\r\n"
 				+ "Program on Master Branch");
-		
+		//constant
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		//variable
+		int empHrs = 0;
+		int empWage = 0;
+	
 		//Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_FULL_TIME)
+		if(empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is present!!: " + empCheck);
-		else
+		    empHrs = 8;
+		}
+		else {
 			System.out.println("Employee is Absent!!: " + empCheck);
-
+			empHrs = 0;
+		}
+		
+		empWage = empHrs * EMP_RATE_PER_HOUR; 
+	    System.out.println("Employee wage: " + empWage);
+			
 	}
 
 }
