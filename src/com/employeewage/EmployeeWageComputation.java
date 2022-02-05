a9384ab62be5075c5f9a34018c705a4a7cdf9aaf
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-05 22:36:47 +0530

    Added: part time logic

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -9,21 +9,22 @@ public class EmployeeWageComputation {
 				+ "Program on Master Branch");
 		//constant
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		//variable
 		int empHrs = 0;
 		int empWage = 0;
 	
 		//Computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if(empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is present!!: " + empCheck);
 		    empHrs = 8;
 		}
-		else {
-			System.out.println("Employee is Absent!!: " + empCheck);
-			empHrs = 0;
+		else if(empCheck == IS_PART_TIME) {
+			empHrs = 4;
 		}
+		else
+			empHrs = 0;
 		
 		empWage = empHrs * EMP_RATE_PER_HOUR; 
 	    System.out.println("Employee wage: " + empWage);
