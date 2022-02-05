75478a6e70041e8e2bea0f93fbd9279d2432ca3c
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-05 21:57:35 +0530

    Added: Employee is present or not logic

---
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -0,0 +1,21 @@
+package com.employeewage;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Start with Displaying\r\n"
+				+ "Welcome to Employee\r\n"
+				+ "Wage Computation\r\n"
+				+ "Program on Master Branch");
+		
+		int IS_FULL_TIME = 1;
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present: " + empCheck);
+		else
+			System.out.println("Employee is Absent: " + empCheck);
+
+	}
+
+}
