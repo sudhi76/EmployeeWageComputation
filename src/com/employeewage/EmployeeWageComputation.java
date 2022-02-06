0de79e131b06df34c60accbac52a3768dcb18720
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-06 22:20:42 +0530

    Added: switch case logic

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -8,23 +8,25 @@ public class EmployeeWageComputation {
 				+ "Wage Computation\r\n"
 				+ "Program on Master Branch");
 		//constant
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
+		final int EMP_RATE_PER_HOUR = 20;
 		//variable
 		int empHrs = 0;
 		int empWage = 0;
 	
 		//Computation
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if(empCheck == IS_FULL_TIME) {
-		    empHrs = 8;
-		}
-		else if(empCheck == IS_PART_TIME) {
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+			case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		case IS_PART_TIME:
 			empHrs = 4;
-		}
-		else
+			break;
+		default:
 			empHrs = 0;
+	}
 		
 		empWage = empHrs * EMP_RATE_PER_HOUR; 
 	    System.out.println("Employee wage: " + empWage);
