18f8e5b9dbcaf629e0682367aad8bb607a2e2f4c
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-06 22:36:30 +0530

    Added: Calculated wage for a month

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -11,26 +11,30 @@ public class EmployeeWageComputation {
 		final int IS_FULL_TIME = 1;
 		final int IS_PART_TIME = 2;
 		final int EMP_RATE_PER_HOUR = 20;
+		final int MAX_WORKING_DAYS = 20;
 		//variable
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalworkingDays = 0;
 	
-		//Computation
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-			case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		default:
-			empHrs = 0;
-	}
-		
-		empWage = empHrs * EMP_RATE_PER_HOUR; 
-	    System.out.println("Employee wage: " + empWage);
-			
-	}
+			//Computing salary for 20 days
+				while (totalworkingDays < MAX_WORKING_DAYS) {
+					int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+					totalworkingDays++;
+					switch(empCheck) {
+						case 1: empHrs=8;
+								break;
+						case 2: empHrs=4;
+								break;
+						default: empHrs=0;
+								break;	
+				}
+					empWage = EMP_RATE_PER_HOUR * empHrs;
+					totalEmpWage += empWage;
+					
+			}
+				System.out.println("Total wage for a month = " + totalEmpWage);
+			}
 
-}
+		}
